cbb5418e2ce900c7d30c879a0804e8e07824788b
dennis.zhang <dennis.zhang@sap.com>
2019-12-12 17:53:54 +0800

    change some error

--- 01_lesson/src/App.js
+++ 01_lesson/src/App.js
@@ -1,11 +1,11 @@
 import React ,{useState} from 'react';
-import {Router} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import Index from './container/index'
 import About from './container/About'
 
 export default (
     <div>
-        <Router path='/' exact component={Index}></Router>
-        <Router path='/About' exact component={About}></Router>
+        <Route path='/' exact component={Index}></Route>
+        <Route path='/About' exact component={About}></Route>
     </div>
 )
