621a360639f39a2f83b600369747705735377118
zhanghqq11 <38965310+zhanghqq11@users.noreply.github.com>
2019-12-16 21:12:33 +0800

    add assignment solution

--- 01_lesson/server/index.js
+++ 01_lesson/server/index.js
@@ -1,66 +1,84 @@
-// 这里的node代码，会用babel处理
 import React from 'react'
-import {renderToString} from 'react-dom/server'
-import express from 'express'
-import {StaticRouter,matchPath,Route} from 'react-router-dom'
-import {Provider} from 'react-redux'
-import routes from '../src/App'
+import { renderToString } from 'react-dom/server'
+import {StaticRouter, matchPath, Route }  from 'react-router-dom'
 import {getServerStore} from '../src/store/store'
 import Header from '../src/component/Header'
+import {Provider} from 'react-redux'
+import express from 'express'
+import routes from '../src/App'
+import axios from 'axios'
+
+const app = express()
 
 const store = getServerStore()
-const app = express()
-app.use(express.static('pubilc'))
 
-app.get('*',(req,res)=>{
-    // 获取根据路由渲染出得组件，并且拿到loadData方法 获取数据。
-    // inside a request
-    const promises = [];
-    // use `some` to imitate `<Switch>` behavior of selecting only
-    // the first to match
-    routes.some(route => {
-    // use `matchPath` here
-        const match = matchPath(req.path, route);
-        if (match) {
-            const {loadData} = route.component
-            if(loadData){
-                promises.push(loadData(store))
-            }
-        }
-        return match;
-    });
+app.use(express.static('public'))
 
-    Promise.all(promises).then(data => {
-        // do something w/ the data so the client
-        // can access it then render the app
-        // const Page = <App title="dennis"></App>
-        // react =>解析成html
-        const content = renderToString(
-                <Provider store = {store}>
-                    <StaticRouter location={req.url}> 
-                        <Header></Header>
-                        {routes.map(route=><Route {...route}></Route>)}
-                    </StaticRouter>
-                </Provider>
-            )
-            res.send(`<html>
-                <head>
-                    <meta charset="utf-8"/>
+app.get('/app/*', (req, res) => {
+  axios.request({
+    method: req.method.toLocaleLowerCase(),
+    baseURL:'http://localhost:9090',
+    url:req.url,
+    data:req.body
+  }).then(resovle => {
+    res.send(resovle.data)
+  }).catch(err => {
+    console.log('err', err)
+  })
+})
+app.get('*', (req, res) => {
+  const promises = []
+  const myPromise = promiseFn => {
+    return new Promise((resolve) => {
+      resolve(promiseFn())
+    }).then(res => {
+      console.log('res', res)
+    }).catch(err => {
+      console.log('err', err)
+    })
+  }
+  const rList = promises.map( item => myPromise(item))
+  routes.some(route => {
+    const match = matchPath(req.path, route)
+    if (match) {
+      const {loadData} = route.component
+      if (loadData) {
+        promises.push(loadData(store))
+      }
+    }
+  })
+  Promise.all(rList).then(() => {
+    const content = renderToString(
+      <Provider store={store}>
+        <StaticRouter location={req.url}>
+          <Header></Header>
+          {
+            routes.map(route => <Route {...route}></Route>)
+          }
+        </StaticRouter>
+      </Provider>
+    )
+    res.send(`
+      <html>
+        <head>
+          <meta charset="utf-8" />
+          <title>My react ssr</title>
+        </head>
+        <body>
+          <div id="root">${content}</div>
+          <script>
+            window.__context = ${JSON.stringify(store.getState())}
+          </script>
+          <script src="/bundle.js"></script>
+        </body>
+      </html>
+    `)
+  }).catch(err => {
+    res.send('页面报错500')
+  })
 
-                    <title>react ssr</title>
-                </head>
-                <body>
-                    <div id="root">${content}</div>
-                        <script>
-                            window.__content=${JSON.stringify(store.getState())}
-                        </script>
-                    <script src="/bundle.js"></script>
-                </body>
-            </html>`)
-        })
-    });
-    
-app.listen(9093,()=>{
-    console.log("监听done")
 })
 
+app.listen(9093, _ => {
+  console.log('监听done')
+})
